5f3ad5ef4ff723ec3e53cc50f2dfd2301c713071
ygrillo <ygrillo@tutanota.com>
2025-02-09 20:10:26 -0300

    initial commit

---
+++ src/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Yuri"
+	idade := 29
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome, ". Sua idade é", idade, "anos")
+	fmt.Println("Este programa está na versão", versao)
+}
