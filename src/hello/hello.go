2ed5461ca83d8c76ba1b5f6f30757b4e3169bd18
ygrillo <ygrillo@tutanota.com>
2025-02-09 20:26:28 -0300

    feat: using scan function to capture input from user

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	nome := "Yuri"
-	idade := 29
 	versao := 1.1
-	fmt.Println("Olá, Sr.", nome, ". Sua idade é", idade, "anos")
+	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir os logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da variável comando é", &comando)
+	fmt.Println("O comando escolhi foi", comando)
 }
